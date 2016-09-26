23c70ebd00278d506e915e1ff334ff3566666e34
felix <felix.dietrich@student.uibk.ac.at>
2016-09-26 11:15:11 +0200

    backroll to server-side strong filtering

--- src/main/java/eu/transkribus/solrSearch/TrpSearcher.java
+++ src/main/java/eu/transkribus/solrSearch/TrpSearcher.java
@@ -104,7 +104,7 @@ public class TrpSearcher {
 
 		if (filters != null) {
 			for (String filter : filters) {
-				filterString += "OR " + filter + " ";
+				filterString += "AND " + filter + " ";
 			}
 
 		}
